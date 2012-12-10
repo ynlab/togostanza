f2374f124676710fbba36eca7d574ccdebfd8170
Naoki Nishiguchi <nnishiguchister@gmail.com>
2012-12-10 15:16:20 +0900

    rails new TogoStanza -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TogoStanza::Application.config.secret_token = 'af5f10b3054e7ba6659546305c38499de62e0d69697be378f1eb089e0a5ee8c75582dc988840ba4d449580c4d12c8186fd70494ef60628379ce9693998c14a83'
