61e5e6c58d91bc723d036f54d7dcd1e0d953e7d7
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-01-15 11:51:41 +0900

    構造を変えてみた

--- app/stanza/protain_names_and_origin_stanza.rb
+++ app/stanza/protain_names_and_origin_stanza.rb
@@ -1,11 +1,10 @@
 class ProtainNamesAndOriginStanza < StanzaBase
   property :summary do |gene_id|
-    solutions = query(:uniprot, <<-SPARQL)
+    protain_summary = query(:uniprot, <<-SPARQL)
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       PREFIX up: <http://purl.uniprot.org/core/>
-      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
-      SELECT DISTINCT ?recommended_name ?ec_name ?alternative_names ?encoded_by ?gene_names ?synonyms_name ?locus_name ?organism_name ?taxonomic_identifier ?parent_taxonomy_name
+      SELECT DISTINCT ?recommended_name ?ec_name ?alternative_names ?organism_name ?taxonomic_identifier ?parent_taxonomy_names
       WHERE {
         ?target up:locusName "#{gene_id}" .
         ?id up:encodedBy ?target .
@@ -21,15 +20,6 @@ class ProtainNamesAndOriginStanza < StanzaBase
         ?id up:alternativeName ?alternative_names_node .
         ?alternative_names_node up:fullName ?alternative_names .
 
-        # Gene names
-        ?id up:encodedBy ?encoded_by .
-        ## Name:
-        ?encoded_by skos:prefLabel ?gene_names .
-        ## Synonyms:
-        ?encoded_by skos:altLabel ?synonyms_name .
-        ## Ordered Locus Names:
-        ?encoded_by up:locusName ?locus_name .
-
         # Organism
         ?id up:organism ?taxonomy_id .
 
@@ -40,16 +30,43 @@ class ProtainNamesAndOriginStanza < StanzaBase
 
         # Taxonomic lineage
         ?taxonomy_id rdfs:subClassOf* ?parent_taxonomy .
-        ?parent_taxonomy up:scientificName ?parent_taxonomy_name .
+        ?parent_taxonomy up:scientificName ?parent_taxonomy_names .
       }
     SPARQL
 
-    # [{a: 'hoge', b: 'moge'}, {a: 'hoge', b: 'fuga'}] => {a: ['hoge'], b: ['moge', 'fuga']}
-    solutions.inject({}) {|new_hash, old_hash|
+    genes = query(:uniprot, <<-SPARQL)
+      PREFIX up: <http://purl.uniprot.org/core/>
+      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+      SELECT DISTINCT ?gene_name ?synonyms_name ?locus_name
+      WHERE {
+        ?target up:locusName "#{gene_id}" .
+        ?id up:encodedBy ?target .
+
+        # Gene names
+        ?id up:encodedBy ?encoded_by .
+        ## Name:
+        ?encoded_by skos:prefLabel ?gene_name .
+        ## Synonyms:
+        ?encoded_by skos:altLabel ?synonyms_name .
+        ## Ordered Locus Names:
+        ?encoded_by up:locusName ?locus_name .
+      }
+    SPARQL
+
+    # [{a: 'hoge', b: 'moge'}, {a: 'hoge', b: 'fuga'}] => {a: 'hoge', b: ['moge', 'fuga']}
+    protain_summary.inject({}) {|new_hash, old_hash|
       old_hash.each {|k, v|
-        (new_hash[k] ||= []) << v.to_s unless new_hash[k] && new_hash[k].include?(v.to_s)
+        case new_hash[k]
+        when Array
+          new_hash[k] << v.to_s unless new_hash[k].include?(v.to_s)
+        when String
+          new_hash[k] = [new_hash[k], v.to_s] if new_hash[k] != v.to_s
+        when nil
+          new_hash[k] = v.to_s
+        end
       }
       new_hash
-    }
+    }.merge(genes: genes)
   end
 end
