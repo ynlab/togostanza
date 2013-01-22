e5efa12cd1dcac432b34fec85f1937648e72af51
Keita Urashima <ursm@ursm.jp>
2013-01-23 01:05:49 +0900

    切り離せるものでもない

--- lib/stanza/base.rb
+++ lib/stanza/base.rb
@@ -3,6 +3,26 @@ require 'stanza/querying'
 require 'stanza/markdown'
 
 module Stanza
+  class << self
+    def find(id)
+      "#{id.camelize}Stanza".constantize
+    end
+
+    def all
+      Stanza::Base.descendants
+    end
+
+    def load_all!
+      Dir[root.join('*_stanza.rb')].each do |f|
+        require f
+      end
+    end
+
+    def root
+      Rails.root.join('app/stanza')
+    end
+  end
+
   class Base
     include Context
     include Querying
