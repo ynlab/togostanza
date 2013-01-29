e681e8435d0690df34e5ff1cbd838de3ae5a901c
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-01-29 19:51:20 +0900

    refactorings

--- app/stanza/protein_sequences_stanza.rb
+++ app/stanza/protein_sequences_stanza.rb
@@ -4,41 +4,31 @@ class ProteinSequencesStanza < Stanza::Base
   end
 
   property :sequences do |gene_id|
-    uniprot_url = query(:togogenome, <<-SPARQL).first[:up]
-      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX insdc: <http://rdf.insdc.org/>
-
-      SELECT ?up
-      WHERE {
-        ?s insdc:feature_locus_tag "#{gene_id}" .
-        ?s rdfs:seeAlso ?np .
-        ?np rdf:type insdc:Protein .
-        ?np rdfs:seeAlso ?up .
-      }
-    SPARQL
-
     sequences = query(:uniprot, <<-SPARQL)
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
       PREFIX up: <http://purl.uniprot.org/core/>
 
       SELECT DISTINCT ?protein ?value ?mass ?modified ?version ?checksum
       WHERE {
-        ?protein rdfs:seeAlso <#{uniprot_url}> .
-        ?protein up:reviewed true .
+        ?protein rdfs:seeAlso <#{uniprot_url_from_togogenome(gene_id)}> ;
+                 up:reviewed true .
 
         ?protein up:sequence ?seq .
-        ?seq rdf:value ?value .
-        ?seq up:mass ?mass .
-        ?seq up:modified ?modified .
-        ?seq up:version ?version .
-        ?seq up:crc64Checksum ?checksum.
+        ?seq rdf:value ?value ;
+             up:mass ?mass ;
+             up:modified ?modified ;
+             up:version ?version ;
+             up:crc64Checksum ?checksum .
       }
     SPARQL
 
-    sequences.map {|sequence|
-      sequence.merge(
-        sequence_length: sequence[:value].size,
-        mass: ActionController::Base.helpers.number_with_delimiter(sequence[:mass].to_i)
+    sequences.map {|hash|
+      value, protein = hash.values_at(:value, :protein)
+
+      hash.merge(
+        sequence_length: value.size,
+        # 'http://purl.uniprot.org/uniprot/P72587' => 'P72587'
+        up_id: protein.split('/').last
       )
     }
   end
