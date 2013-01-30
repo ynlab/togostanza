2f59560e28603a04950440a50dd3e0ed5452f476
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-01-30 13:54:37 +0900

    refactorings

--- app/stanza/protein_references_stanza.rb
+++ app/stanza/protein_references_stanza.rb
@@ -5,24 +5,24 @@ class ProteinReferencesStanza < Stanza::Base
 
   property :references do |gene_id|
     references = query(:uniprot, <<-SPARQL)
-      PREFIX up: <http://purl.uniprot.org/core/>
+      PREFIX up:   <http://purl.uniprot.org/core/>
       PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
       SELECT DISTINCT ?pmid ?title (GROUP_CONCAT(DISTINCT ?author; SEPARATOR=", ") AS ?authors) ?date ?name ?pages ?volume ?same
       WHERE {
         ?protein rdfs:seeAlso <#{uniprot_url_from_togogenome(gene_id)}> ;
-                 up:reviewed true .
+                 up:reviewed  true .
 
-        ?protein up:citation ?citation .
+        ?protein  up:citation     ?citation .
         ?citation skos:exactMatch ?pmid .
-        FILTER regex (str(?pmid), "pubmed") .
+        FILTER    regex (str(?pmid), "pubmed") .
 
-        ?citation up:title ?title ;
-                  up:author ?author ;
-                  up:date ?date ;
-                  up:name ?name ;
-                  up:pages ?pages ;
-                  up:volume ?volume ;
+        ?citation up:title   ?title ;
+                  up:author  ?author ;
+                  up:date    ?date ;
+                  up:name    ?name ;
+                  up:pages   ?pages ;
+                  up:volume  ?volume ;
                   owl:sameAs ?same .
       }
       GROUP BY ?pmid ?title ?date ?name ?pages ?volume ?same
