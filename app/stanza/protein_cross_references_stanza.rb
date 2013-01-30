2e6158473aa0c873050728744f9671e327f6da17
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-01-30 13:50:02 +0900

    refactorings

--- app/stanza/protein_cross_references_stanza.rb
+++ app/stanza/protein_cross_references_stanza.rb
@@ -7,32 +7,32 @@ class ProteinCrossReferencesStanza < Stanza::Base
     references = query(:uniprot, <<-SPARQL)
       PREFIX up: <http://purl.uniprot.org/core/>
 
-      SELECT DISTINCT ?protein ?category ?abbreviation ?ref ?url_temp
+      SELECT DISTINCT ?protein ?category ?abbr ?ref ?url_template
       WHERE {
-        ?protein rdfs:seeAlso <#{uniprot_url_from_togogenome(gene_id)}> ;
-                 up:reviewed true .
+        ?protein  rdfs:seeAlso    <#{uniprot_url_from_togogenome(gene_id)}> ;
+                  up:reviewed     true .
 
-        ?protein rdfs:seeAlso ?ref .
-        ?ref up:database ?database .
-        ?database up:category ?category ;
-                  up:abbreviation ?abbreviation ;
-                  up:UrlTemplate ?url_temp .
+        ?protein  rdfs:seeAlso    ?ref .
+        ?ref      up:database     ?database .
+        ?database up:category     ?category ;
+                  up:abbreviation ?abbr ;
+                  up:UrlTemplate  ?url_template .
       }
     SPARQL
 
-    # merge でデータを付けた後、category、abbreviation でグループ化している
-    # [{category: 'xxx', abbreviation: 'hoge', ref: 'aaa'}, {category: 'xxx', abbreviation: 'hoge', ref: 'bbb'}, {category: 'xxx', abbreviation: 'moge', ref: 'ccc'}, {category: 'yyy', abbreviation: 'fuga', ref: 'ddd'}]
-    # => [[[{:category=>"xxx", :abbreviation=>"hoge", :ref=>"aaa"}, {:category=>"xxx", :abbreviation=>"hoge", :ref=>"bbb"}], [{:category=>"xxx", :abbreviation=>"moge", :ref=>"ccc"}]], [[{:category=>"yyy", :abbreviation=>"fuga", :ref=>"ddd"}]]]
+    # merge でデータを付けた後、category、abbr でグループ化している
+    # [{category: 'xxx', abbr: 'hoge', ref: 'aaa'}, {category: 'xxx', abbr: 'hoge', ref: 'bbb'}, {category: 'xxx', abbr: 'moge', ref: 'ccc'}, {category: 'yyy', abbr: 'fuga', ref: 'ddd'}]
+    # => [[[{:category=>"xxx", :abbr=>"hoge", :ref=>"aaa"}, {:category=>"xxx", :abbr=>"hoge", :ref=>"bbb"}], [{:category=>"xxx", :abbr=>"moge", :ref=>"ccc"}]], [[{:category=>"yyy", :abbr=>"fuga", :ref=>"ddd"}]]]
     references.map {|hash|
       up_id  = hash[:protein].split('/').last
       ref_id = hash[:ref].split('/').last
 
       hash.merge(
         ref_id: ref_id,
-        url:    hash[:url_temp].gsub(/%s|%u/, "%s" => ref_id, "%u" => up_id)
+        url:    hash[:url_template].gsub(/%s|%u/, "%s" => ref_id, "%u" => up_id)
       )
     }.reverse.group_by {|hash| hash[:category] }.map {|hash|
-      hash.last.group_by {|h| h[:abbreviation] }.values
+      hash.last.group_by {|h| h[:abbr] }.values
     }
   end
 end
