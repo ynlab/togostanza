5dd839577db878c9880377d215d5032e4e40304c
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-01-30 10:53:03 +0900

    refactorings

--- app/stanza/protein_references_stanza.rb
+++ app/stanza/protein_references_stanza.rb
@@ -4,40 +4,26 @@ class ProteinReferencesStanza < Stanza::Base
   end
 
   property :references do |gene_id|
-    uniprot_url = query(:togogenome, <<-SPARQL).first[:up]
-      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX insdc: <http://rdf.insdc.org/>
-
-      SELECT ?up
-      WHERE {
-        ?s insdc:feature_locus_tag "#{gene_id}" .
-        ?s rdfs:seeAlso ?np .
-        ?np rdf:type insdc:Protein .
-        ?np rdfs:seeAlso ?up .
-      }
-    SPARQL
-
     references = query(:uniprot, <<-SPARQL)
       PREFIX up: <http://purl.uniprot.org/core/>
       PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
-      SELECT DISTINCT ?pmid ?title (GROUP_CONCAT(DISTINCT ?author; SEPARATOR=",") AS ?authors) ?date ?name ?pages ?volume ?same
+      SELECT DISTINCT ?pmid ?title (GROUP_CONCAT(DISTINCT ?author; SEPARATOR=", ") AS ?authors) ?date ?name ?pages ?volume ?same
       WHERE {
-        ?protein rdfs:seeAlso <#{uniprot_url}> .
-        ?protein up:reviewed true .
+        ?protein rdfs:seeAlso <#{uniprot_url_from_togogenome(gene_id)}> ;
+                 up:reviewed true .
 
         ?protein up:citation ?citation .
         ?citation skos:exactMatch ?pmid .
-        FILTER regex (str(?pmid), "pubmed")
+        FILTER regex (str(?pmid), "pubmed") .
 
-        ?citation up:title ?title .
-        ?citation up:author ?author .
-        ?citation up:date ?date .
-        ?citation up:name ?name .
-        ?citation up:pages ?pages .
-        ?citation up:volume ?volume .
-        ?citation <http://www.w3.org/2002/07/owl#sameAs> ?same
+        ?citation up:title ?title ;
+                  up:author ?author ;
+                  up:date ?date ;
+                  up:name ?name ;
+                  up:pages ?pages ;
+                  up:volume ?volume ;
+                  owl:sameAs ?same .
       }
       GROUP BY ?pmid ?title ?date ?name ?pages ?volume ?same
       ORDER BY ?date
