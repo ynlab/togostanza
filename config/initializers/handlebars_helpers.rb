11a66c1bd47a7408219474154d83d0180676da51
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-02-05 15:29:14 +0900

    クロスオリジンの時 iframe の高さの自動調節が効かない不具合を解消
    * クロスオリジンの時は、iframe 内から親Window へのアクセスが出来ない
    * Web Messageing で iframe 内から高さと自身のname を親へ送り、親側で iframe の height に設定している

--- config/initializers/handlebars_helpers.rb
+++ config/initializers/handlebars_helpers.rb
@@ -1,5 +1,8 @@
 FS.register_helper :adjust_iframe_height_script do
   <<-HTML.strip_heredoc.html_safe
-    <script>$(function() { frameElement.height = frameElement.contentDocument.body.offsetHeight + 53; });</script>
+    <script>$(function() {
+      height = this.body.offsetHeight + 30;
+      parent.postMessage(JSON.stringify({height: height, id: name}), "*");
+    });</script>
   HTML
 end
