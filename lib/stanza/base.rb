61f2c7c2cd2490123d681867534d5b10b3bf6d6f
Keita Urashima <ursm@ursm.jp>
2013-03-06 17:54:27 +0900

    Move uniprot_url_from_togogenome method to Stanza::Querying

--- lib/stanza/base.rb
+++ lib/stanza/base.rb
@@ -60,21 +60,5 @@ module Stanza
 
       Markdown.render(path.read)
     end
-
-    def uniprot_url_from_togogenome(gene_id)
-      # refseq の UniProt
-      # slr1311 の時 "http://purl.uniprot.org/refseq/NP_439906.1"
-      query(:togogenome, <<-SPARQL).first[:up]
-        PREFIX insdc: <http://rdf.insdc.org/>
-
-        SELECT ?up
-        WHERE {
-          ?s insdc:feature_locus_tag "#{gene_id}" .
-          ?s rdfs:seeAlso ?np .
-          ?np rdf:type insdc:Protein .
-          ?np rdfs:seeAlso ?up .
-        }
-      SPARQL
-    end
   end
 end
