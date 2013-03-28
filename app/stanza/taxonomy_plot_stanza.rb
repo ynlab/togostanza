aaf2083194812115650995d2c8bd7c35baceb01f
Keita Urashima <ursm@ursm.jp>
2013-03-28 15:33:08 +0900

    Cosmetic

--- app/stanza/taxonomy_plot_stanza.rb
+++ app/stanza/taxonomy_plot_stanza.rb
@@ -1,28 +1,27 @@
 class TaxonomyPlotStanza < Stanza::Base
-
-  property (:selected_taxonomy_id) do |tax_id|
-    "#{tax_id}"
+  property :selected_taxonomy_id do |tax_id|
+    tax_id
   end
 
   resource :taxonomy do
-    sequence_list = Array::new
-    habitat_list = Array::new
-    phenotype_list = Array::new
-    tax_label_list = Array::new
+    sequence_list  = []
+    habitat_list   = []
+    phenotype_list = []
+    tax_label_list = []
 
-    sequence_list =  query("http://lod.dbcls.jp/openrdf-sesame5l/repositories/togogenome",<<-SPARQL.strip_heredoc)
+    sequence_list = query('http://lod.dbcls.jp/openrdf-sesame5l/repositories/togogenome',<<-SPARQL.strip_heredoc)
       PREFIX obo: <http://purl.obolibrary.org/obo/>
       PREFIX insdc: <http://rdf.insdc.org/>
-   
+
       SELECT ?tax ?size ?num_gene ?num_trna ?num_rrna
-      WHERE 
+      WHERE
       {
         {
           SELECT ?tax (COUNT(?gene) AS ?num_gene) (COUNT(?trna) AS ?num_trna) (COUNT(?rrna) AS ?num_rrna)
-          WHERE 
+          WHERE
           {
             ?seq rdf:type ?obo_type ;
-              rdfs:seeAlso ?tax 
+              rdfs:seeAlso ?tax
               FILTER (?obo_type = obo:SO_0000340 || ?obo_type = obo:SO_0000155) .
             ?tax rdf:type insdc:Taxonomy .
             { ?gene obo:so_part_of ?seq ; rdf:type obo:SO_0000704 . }
@@ -35,38 +34,39 @@ class TaxonomyPlotStanza < Stanza::Base
         }
         {
           SELECT ?tax (SUM(?len) AS ?size)
-          WHERE 
+          WHERE
           {
             ?seq rdf:type ?obo_type ;
               insdc:sequence_length ?len ;
-              rdfs:seeAlso ?tax 
+              rdfs:seeAlso ?tax
               FILTER (?obo_type = obo:SO_0000340 || ?obo_type = obo:SO_0000155) .
             ?tax rdf:type insdc:Taxonomy .
           }
-          GROUP BY ?tax 
+          GROUP BY ?tax
         }
       }
     SPARQL
 
-# SPARQL statement for filtering by tax_id
-    tax_filter = "FILTER (?tax IN ("
-    sequence_list.each_with_index { |entity, idx|
+    # SPARQL statement for filtering by tax_id
+    tax_filter = 'FILTER (?tax IN ('
+
+    sequence_list.each_with_index do |entity, idx|
       s = entity[:tax]
-      tax_filter << "tax:" << s.slice( s.rindex("/")+1 ,s.length )
-      if idx < sequence_list.size - 1
-        tax_filter << ", "
-      end
-    }
-    tax_filter << "))"
-    
-    query2 = Thread.new do
-      habitat_list =  query("http://lod.dbcls.jp/openrdf-sesame5l/repositories/gold2",<<-SPARQL.strip_heredoc)
+      tax_filter << 'tax:' << s.slice(s.rindex('/') + 1, s.length)
+
+      tax_filter << ', ' if idx < sequence_list.size - 1
+    end
+
+    tax_filter << '))'
+
+    query2 = Thread.new {
+      habitat_list =  query('http://lod.dbcls.jp/openrdf-sesame5l/repositories/gold2', <<-SPARQL.strip_heredoc)
         PREFIX meo: <http://purl.jp/bio/11/meo/>
         PREFIX mccv: <http://purl.jp/bio/01/mccv#>
         PREFIX tax: <http://identifiers.org/taxonomy/>
-        
+
         SELECT ?tax (GROUP_CONCAT(distinct(?label2); SEPARATOR=",") as ?habitat)
-        WHERE 
+        WHERE
         {
           ?gold meo:environmentalDescribed ?envi .
           ?gold mccv:MCCV_000020 ?tax #{tax_filter} .
@@ -77,10 +77,10 @@ class TaxonomyPlotStanza < Stanza::Base
         }
         GROUP BY ?tax ORDER BY ?tax ?meo2
       SPARQL
-    end
+    }
 
-    query3 = Thread.new do
-      phenotype_list =  query("http://lod.dbcls.jp/openrdf-sesame5l/repositories/gold2",<<-SPARQL.strip_heredoc)
+    query3 = Thread.new {
+      phenotype_list =  query('http://lod.dbcls.jp/openrdf-sesame5l/repositories/gold2', <<-SPARQL.strip_heredoc)
         PREFIX mpo:<http://purl.jp/bio/01/mpo#>
         PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
         PREFIX tax: <http://identifiers.org/taxonomy/>
@@ -89,92 +89,95 @@ class TaxonomyPlotStanza < Stanza::Base
         WHERE
         {
           ?tax ?p ?o #{tax_filter} .
-          OPTIONAL 
+          OPTIONAL
           {
             ?tax mpo:MPO_10001 ?cell_shape.
             ?cell_shape skos:prefLabel ?cell_shape_label .
           }
-          OPTIONAL 
+          OPTIONAL
           {
             ?tax mpo:MPO_10003 ?temp_range.
             ?temp_range skos:prefLabel ?temp_range_label .
           }
           OPTIONAL { ?tax mpo:MPO_10009 ?opt_temp . }
-          OPTIONAL 
-          { 
+          OPTIONAL
+          {
             ?tax mpo:MPO_10010 ?min_temp .
             ?tax mpo:MPO_10011 ?max_temp .
           }
-          OPTIONAL 
+          OPTIONAL
           {
             ?tax mpo:MPO_10002 ?oxy_req .
             ?oxy_req skos:prefLabel ?oxy_req_label .
           }
           OPTIONAL { ?tax mpo:MPO_10005 ?opt_ph . }
-          OPTIONAL 
+          OPTIONAL
           {
             ?tax mpo:MPO_10006 ?min_ph .
             ?tax mpo:MPO_10007 ?max_ph .
           }
         }
       SPARQL
-    end
-    
-    query4 = Thread.new do
-      tax_label_list =  query("http://lod.dbcls.jp/openrdf-sesame5l/repositories/ncbitaxon",<<-SPARQL.strip_heredoc)
+    }
+
+    query4 = Thread.new {
+      tax_label_list =  query('http://lod.dbcls.jp/openrdf-sesame5l/repositories/ncbitaxon', <<-SPARQL.strip_heredoc)
         PREFIX tax: <http://purl.obolibrary.org/obo/NCBITaxon_>
 
         SELECT ?tax ?label
-        WHERE 
+        WHERE
         {
           ?tax rdfs:label ?label #{tax_filter} .
         }
       SPARQL
-    end
+    }
 
     query2.join
     query3.join
     query4.join
 
-#merge sequence data with habitat data
-    habitat_hash = Hash::new
-    habitat_list.each { |i|
+    # merge sequence data with habitat data
+    habitat_hash = {}
+
+    habitat_list.each do |i|
       habitat_hash[i[:tax]] = i
-    }
+    end
 
-    sequence_list.each { |i|
-        if habitat_hash[i[:tax]] && habitat_hash[i[:tax]][:habitat]
-          i[:habitat] = habitat_hash[i[:tax]][:habitat]
-        end
-    }
+    sequence_list.each do |i|
+      if habitat_hash[i[:tax]] && habitat_hash[i[:tax]][:habitat]
+        i[:habitat] = habitat_hash[i[:tax]][:habitat]
+      end
+    end
 
-#merge taxonomy sequence data with phenotype data
-    phenotype_hash = Hash::new
-    phenotype_list.each { |i|
+    # merge taxonomy sequence data with phenotype data
+    phenotype_hash = {}
+
+    phenotype_list.each do |i|
       phenotype_hash[i[:tax]] = i
-    }
+    end
 
     items = [:cell_shape_label, :temp_range_label, :oxy_req_label, :opt_temp, :min_temp, :max_temp, :opt_ph, :min_ph, :max_ph ]
-    sequence_list.each { |i|
-      items.each { |item|
+
+    sequence_list.each do |i|
+      items.each do |item|
         if phenotype_hash[i[:tax]] && phenotype_hash[i[:tax]][item]
           i[item] = phenotype_hash[i[:tax]][item]
         end
-      }
-    }
+      end
+    end
 
-#merge sequence data with taxonomy label
-    tax_label_hash = Hash::new
-    tax_label_list.each { |i|
-      tax_id = i[:tax].gsub("http://purl.obolibrary.org/obo/NCBITaxon_", "http://identifiers.org/taxonomy/")
+    # merge sequence data with taxonomy label
+    tax_label_hash = {}
+
+    tax_label_list.each do |i|
+      tax_id = i[:tax].gsub('http://purl.obolibrary.org/obo/NCBITaxon_', 'http://identifiers.org/taxonomy/')
       tax_label_hash[tax_id] = i
-    }
+    end
 
-    sequence_list.each { |i|
-        if tax_label_hash[i[:tax]] && tax_label_hash[i[:tax]][:label]
-          i[:label] = tax_label_hash[i[:tax]][:label]
-        end
-    }
-
+    sequence_list.each do |i|
+      if tax_label_hash[i[:tax]] && tax_label_hash[i[:tax]][:label]
+        i[:label] = tax_label_hash[i[:tax]][:label]
+      end
+    end
   end
 end
