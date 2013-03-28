16c46afc57a646a389fc8505f807836e2204dd82
SemWeb developer <developer@okbp.local>
2013-03-28 17:29:48 +0900

    added graphical view column on protein_sequence_annotaion

--- app/stanza/protein_sequence_annotation_stanza.rb
+++ app/stanza/protein_sequence_annotation_stanza.rb
@@ -6,7 +6,7 @@ class ProteinSequenceAnnotationStanza < Stanza::Base
       PREFIX up: <http://purl.uniprot.org/core/>
       PREFIX taxonomy: <http://purl.uniprot.org/taxonomy/>
 
-      SELECT DISTINCT ?parent_label ?label ?begin_location ?end_location ?comment (GROUP_CONCAT(DISTINCT ?substitution; SEPARATOR=", ") AS ?substitutions) ?seq ?annotation ?feature_identifier
+      SELECT DISTINCT ?parent_label ?label ?begin_location ?end_location ?seq_length ?comment (GROUP_CONCAT(DISTINCT ?substitution; SEPARATOR=", ") AS ?substitutions) ?seq ?annotation ?feature_identifier
       WHERE {
         ?protein up:organism taxonomy:#{tax_id} ;
                  rdfs:seeAlso <#{uniprot_url_from_togogenome(gene_id)}> ;
@@ -26,19 +26,30 @@ class ProteinSequenceAnnotationStanza < Stanza::Base
                up:end ?end_location .
 
         # description の一部が取得できるが、内容の表示に必要があるのか
-        OPTIONAL{
+        OPTIONAL{ 
           ?annotation up:substitution ?substitution .
           ?protein up:sequence/rdf:value ?seq .
-         }
+        }
+        
+        # sequence の長さ取得用
+        OPTIONAL{ 
+          ?protein up:sequence/rdf:value ?seq_txt .
+          BIND (strlen(?seq_txt) as ?seq_length) .
+        }
 
         OPTIONAL {
           BIND (str(?annotation) as ?feature_identifier) .
           FILTER regex(str(?annotation), 'http://purl.uniprot.org/annotation')
         }
       }
-      GROUP BY ?parent_label ?label ?begin_location ?end_location ?comment ?seq ?annotation ?feature_identifier
+      GROUP BY ?parent_label ?label ?begin_location ?end_location ?seq_length ?comment ?seq ?annotation ?feature_identifier
       ORDER BY ?parent_label ?label ?begin_location ?end_location
     SPARQL
+
+    #graphical view 描画用に各行の要素IDを設定
+    annotations.each_with_index{|hash, i|
+      hash['row_id'] = "row" + i.to_s
+    }
 
     annotations.map {|hash|
       begin_location, end_location, substitutions, seq = hash.values_at(:begin_location, :end_location, :substitutions, :seq)
