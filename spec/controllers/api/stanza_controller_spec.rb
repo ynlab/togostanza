e7992b75c59ae67ff2a796071734ef92bbb3343c
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-05-22 14:40:33 +0900

    fix specs

--- spec/controllers/api/stanza_controller_spec.rb
+++ spec/controllers/api/stanza_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Api::StanzaController do
   describe 'GET show' do
     before do
-      get :show, id: 'transcript_attributes', gene_id: 'slr0613'
+      get :show, id: 'protein_names_and_origin', tax_id: '1111708', gene_id: 'slr1311'
     end
 
     it { response.should be_success }
@@ -11,11 +11,37 @@ describe Api::StanzaController do
     it { response.body.should be_json_eql(<<-JSON.strip_heredoc) }
       {
         "css_uri": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.2/css/bootstrap.min.css",
-        "title": "Transcript Attributes : slr0613",
-
-        "transcripts": [
-          {"end_position": "2098", "begin_position": "1577"}
-        ]
+        "genes": [
+          {
+            "gene_name": "psbA3",
+            "locus_name": "sll1867",
+            "synonyms_name": "psbA-3"
+          },
+          {
+            "gene_name": "psbA2",
+            "locus_name": "slr1311",
+            "synonyms_name": "psbA-2"
+          }
+        ],
+        "summary": {
+          "alternative_names": [
+            "Photosystem II protein D1 2",
+            "32 kDa thylakoid membrane protein 2"
+          ],
+          "ec_name": "1.10.3.9",
+          "organism_name": "Synechocystis sp. (strain PCC 6803 / Kazusa)",
+          "parent_taxonomy_names": [
+            "cellular organisms",
+            "Bacteria",
+            "Cyanobacteria",
+            "Chroococcales",
+            "Synechocystis",
+            "Synechocystis sp. PCC 6803",
+            "Synechocystis sp. (strain PCC 6803 / Kazusa)"
+          ],
+          "recommended_name": "Photosystem Q(B) protein 2",
+          "taxonomy_id": "1111708"
+        }
       }
     JSON
   end
--- spec/requests/stanza_spec.rb
+++ spec/requests/stanza_spec.rb
@@ -2,49 +2,15 @@
 
 require 'spec_helper'
 
-feature 'スタンザを表示する' do
-  context 'General Summary スタンザ' do
-    scenario '遺伝子 sll1615' do
-      visit stanza_path('general_summary', gene_id: 'sll1615')
-
-      expect(page).to have_text('tRNA modification GTPase TrmE')
-      expect(page).to have_text('trmE')
-      expect(page).to have_text('mnmE; thdF')
-    end
-  end
-
-  context 'Transcript Attributes スタンザ' do
-    scenario '遺伝子 sll1615' do
-      visit stanza_path('transcript_attributes', gene_id: 'sll1615')
-
-      expect(page).to have_text('1455753')
-      expect(page).to have_text('1457123')
-    end
-  end
-
-  context 'Gene Attributes スタンザ' do
-    scenario '遺伝子 slr1311 / taxid: 1148' do
-      visit stanza_path('gene_attributes', gene_id: 'slr1311', tax_id: '1148')
-
-      expect(page).to have_text('photosystem II D1 protein')
-      expect(page).to have_text('psbA2')
-      expect(page).to have_text('MTTTLQQRESASLWEQFCQWVTSTNNRIYVGWFGTLMIPTLLTATTCFIIAFIAAPPVDIDGIREPVAGSLLYGNNIISGAVVPSSNAIGLHFYPIWEAASLDEWLYNGGPYQLVVFHFLIGIFCYMGRQWELSYRLGMRPWICVAYSAPVSAATAVFLIYPIGQGSFSDGMPLGISGTFNFMIVFQAEHNILMHPFHMLGVAGVFGGSLFSAMHGSLVTSSLVRETTEVESQNYGYKFGQEEETYNIVAAHGYFGRLIFQYASFNNSRSLHFFLGAWPVIGIWFTAMGVSTMAFNLNGFNFNQSILDSQGRVIGTWADVLNRANIGFEVMHERNAHNFPLDLASGEQAPVALTAPAVNG
-')
-      expect(page).to have_text('7229..8311')
-      expect(page).to have_text('http://purl.uniprot.org/refseq/NP_439906.1')
-    end
-  end
-end
-
 feature '表示のカスタマイズ' do
   scenario 'デフォルトでは Bootstrap が使われる' do
-    visit stanza_path('general_summary', gene_id: 'sll1615')
+    visit stanza_path('protein_names_and_origin', tax_id: "1111708", gene_id: 'slr1311')
 
     expect(find('link[rel=stylesheet]')[:href]).to match(%r(/bootstrap.min.css$))
   end
 
   scenario 'css_uri を指定すると、代わりにそれが使われる' do
-    visit stanza_path('general_summary', gene_id: 'sll1615', css_uri: 'http://example.com/my.css')
+    visit stanza_path('protein_names_and_origin', tax_id: "1111708", gene_id: 'slr1311', css_uri: 'http://example.com/my.css')
 
     expect(find('link[rel=stylesheet]')[:href]).to eq('http://example.com/my.css')
   end
