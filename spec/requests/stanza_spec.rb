6daaa084cf42c6422b0c06836db70c536b544836
Keita Urashima <ursm@ursm.jp>
2013-06-20 03:15:47 +0900

    Fix spec

--- spec/requests/stanza_spec.rb
+++ spec/requests/stanza_spec.rb
@@ -3,10 +3,10 @@
 require 'spec_helper'
 
 feature '表示のカスタマイズ' do
-  scenario 'デフォルトでは Bootstrap が使われる' do
+  scenario 'デフォルトでは stanza.css が使われる' do
     visit stanza_path('protein_names_and_origin', tax_id: "1111708", gene_id: 'slr1311')
 
-    expect(find('link[rel=stylesheet]')[:href]).to match(%r(/bootstrap.min.css$))
+    expect(find('link[rel=stylesheet]')[:href]).to match(%r(/stanza.css$))
   end
 
   scenario 'css_uri を指定すると、代わりにそれが使われる' do
