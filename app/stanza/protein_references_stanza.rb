0ecb35e77ec5974b9c76c54a318cb16784a28e3a
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-06-26 17:49:50 +0900

    pretein_references の修正
    - gene_id を slr1311 から sll0018 へ変更
    - GROUP_CONCAT_DISTINCT が使えないが、このクエリーでは重複しないため問題無いため DISTINCT を外した(2つ以上 GROUP_CONCAT する際が危険)
    - GRAPH の指定

--- app/stanza/protein_references_stanza.rb
+++ app/stanza/protein_references_stanza.rb
@@ -5,22 +5,24 @@ class ProteinReferencesStanza < Stanza::Base
       PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
       PREFIX taxonomy: <http://purl.uniprot.org/taxonomy/>
 
-      SELECT DISTINCT ?pmid ?title (GROUP_CONCAT(DISTINCT ?author; SEPARATOR=", ") AS ?authors) ?date ?name ?pages ?volume ?same
+      SELECT DISTINCT ?pmid ?title (GROUP_CONCAT(?author, ", ") AS ?authors) ?date ?name ?pages ?volume ?same
       WHERE {
-        ?protein up:organism  taxonomy:#{tax_id} ;
-                 rdfs:seeAlso <#{uniprot_url_from_togogenome(gene_id)}> .
+        GRAPH <http://togogenome.org/uniprot/> {
+          ?protein up:organism  taxonomy:#{tax_id} ;
+                   rdfs:seeAlso <#{uniprot_url_from_togogenome(gene_id)}> .
 
-        ?protein  up:citation     ?citation .
-        ?citation skos:exactMatch ?pmid .
-        FILTER    regex (str(?pmid), "pubmed") .
+          ?protein  up:citation     ?citation .
+          ?citation skos:exactMatch ?pmid .
+          FILTER    REGEX (STR(?pmid), "pubmed") .
 
-        ?citation up:title   ?title ;
-                  up:author  ?author ;
-                  up:date    ?date ;
-                  up:name    ?name ;
-                  up:pages   ?pages ;
-                  up:volume  ?volume ;
-                  owl:sameAs ?same .
+          ?citation up:title   ?title ;
+                    up:author  ?author ;
+                    up:date    ?date ;
+                    up:name    ?name ;
+                    up:pages   ?pages ;
+                    up:volume  ?volume ;
+                    owl:sameAs ?same .
+        }
       }
       GROUP BY ?pmid ?title ?date ?name ?pages ?volume ?same
       ORDER BY ?date
