b5619e9ec6e2860ee584da2195999740871691f7
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-10-24 11:22:07 +0900

    Generator で生成予定の assets 対応を既存のスタンザに対応した
    - xxx_stanza/lib/xxx_stanza.rb に assets のパスの追加
    - 静的ファイル置き場のディレクトリを用意

--- gene_attributes_stanza/lib/gene_attributes_stanza.rb
+++ gene_attributes_stanza/lib/gene_attributes_stanza.rb
@@ -2,3 +2,5 @@ require 'togostanza'
 require_relative '../stanza'
 
 GeneAttributesStanza.root = File.expand_path('../..', __FILE__)
+
+TogoStanza.sprockets.append_path File.expand_path('../../assets', __FILE__)
