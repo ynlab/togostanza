689cb3b6077e53581dfb6f6747e59f0a7cb54ba1
SemWeb developer <developer@okbp.local>
2013-12-05 19:10:12 +0900

    Adjusted display range of organism_jbrowse and removed DNA tracks from default viewport

--- organism_jbrowse_stanza/stanza.rb
+++ organism_jbrowse_stanza/stanza.rb
@@ -1,11 +1,12 @@
 class OrganismJbrowseStanza < TogoStanza::Stanza::Base
-  property :select_tax_id do |tax_id|
+  property :sequence_version do |tax_id|
     results = query("http://ep.dbcls.jp/sparql7upd2", <<-SPARQL.strip_heredoc)
       DEFINE sql:select-option "order"
       PREFIX obo: <http://purl.obolibrary.org/obo/>
       PREFIX taxid: <http://identifiers.org/taxonomy/>
+      PREFIX ddbj: <http://ddbj.nig.ac.jp/ontologies/sequence#>
 
-      SELECT ?seq
+      SELECT ?version ?length
       FROM <http://togogenome.org/graph/refseq/>
       FROM <http://togogenome.org/graph/so/>
       WHERE
@@ -13,15 +14,19 @@ class OrganismJbrowseStanza < TogoStanza::Stanza::Base
         VALUES ?seq_type  { obo:SO_0000340 obo:SO_0000155 }
 
         ?seq rdfs:seeAlso taxid:#{tax_id} ;
-            rdf:type ?seq_type .
-      }
+          rdf:type ?seq_type ;
+          ddbj:sequence_length ?length ;
+          ddbj:sequence_version ?version .
+      } ORDER BY DESC(?length) LIMIT 1
     SPARQL
 
     if results.length > 0 then
-      select_tax_id = tax_id
+      display_start_pos = 1
+      display_end_pos = [ results.first[:length].to_i, 200000 ].min
+      sequence_version = {:tax_id => tax_id, :ref => results.first[:version], :display_end_pos => display_start_pos, :display_end_pos => display_end_pos}
     else
-      select_tax_id = nil
+      sequence_version = {:tax_id => nil}
     end
-    select_tax_id
+    sequence_version
   end
 end
