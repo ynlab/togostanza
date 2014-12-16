f6a8ca338fd683cc2e2c1bb9e90515a8dd62120a
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-12-16 21:04:13 +0900

    refacotring

--- gmo_applied_spices_stanza/stanza.rb
+++ gmo_applied_spices_stanza/stanza.rb
@@ -1,27 +1,28 @@
 class GmoAppliedSpicesStanza < TogoStanza::Stanza::Base
 	property :applied_spices_list do |medium_id|
-		medium_list = query("http://ep.dbcls.jp/sparql7ssd", <<-SPARQL.strip_heredoc)
+		query("http://ep.dbcls.jp/sparql7ssd", <<-SPARQL.strip_heredoc)
 			PREFIX mccv: <http://purl.jp/bio/01/mccv#>
 			PREFIX gmo: <http://purl.jp/bio/11/gmo#>
-			select ?gmo_title ?label ?tax ?taxonomy_id
-			where {
+			SELECT ?gmo_title ?label ?tax ?taxonomy_id
+			WHERE {
 				?gmo gmo:GMO_000101 "#{medium_id}" .
 				?gmo gmo:GMO_000102 ?gmo_title .
 				?brc mccv:MCCV_000018 ?gmo .
 				?brc mccv:MCCV_000056 ?tax .
 				OPTIONAL { ?tax rdfs:label ?label . }
-				bind('http://identifiers.org/taxonomy/' as ?identifer) .
-				bind( replace(str(?tax), ?identifer, '') as ?taxonomy_id ) .
-				filter( contains(str(?tax),?identifer) )
+				BIND('http://identifiers.org/taxonomy/' AS ?identifer) .
+				BIND( replace(str(?tax), ?identifer, '') AS ?taxonomy_id ) .
+				FILTER( contains(str(?tax),?identifer) )
 			}
-			order by ?label
+			ORDER BY ?label
 		SPARQL
 	end
+
 	property :general_information do |medium_id|
-		medium_list = query("http://ep.dbcls.jp/sparql7ssd", <<-SPARQL.strip_heredoc)
+		query("http://ep.dbcls.jp/sparql7ssd", <<-SPARQL.strip_heredoc)
 			PREFIX gmo: <http://purl.jp/bio/11/gmo#>
-			select ?gmo_title
-			where {
+			SELECT ?gmo_title
+			WHERE {
 				?gmo gmo:GMO_000101 "#{medium_id}" .
 				?gmo gmo:GMO_000102 ?gmo_title .
 			}
