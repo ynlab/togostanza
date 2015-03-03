d5e5fa698fc734098e3630370d8fd962d740c6ca
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-03-03 18:14:41 +0900

    Cosmetic

--- organism_gene_list_stanza/stanza.rb
+++ organism_gene_list_stanza/stanza.rb
@@ -5,7 +5,7 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     end_reactome = "http://www.ebi.ac.uk/rdf/services/reactome/sparql"
 
     ### gene - gene name, position, etc.
-    result = query("#{endpoint}", <<-SPARQL.strip_heredoc)
+    result = query(endpoint, <<-SPARQL.strip_heredoc)
       PREFIX insdc: <http://ddbj.nig.ac.jp/ontologies/nucleotide/>
       PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
       PREFIX faldo: <http://biohackathon.org/resource/faldo#>
@@ -26,7 +26,7 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     next if result.nil?
 
     ### gene - pseudogene
-    gene_pseudo = query("#{endpoint}", <<-SPARQL.strip_heredoc)
+    gene_pseudo = query(endpoint, <<-SPARQL.strip_heredoc)
       PREFIX insdc: <http://ddbj.nig.ac.jp/ontologies/nucleotide/>
       PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
       PREFIX faldo: <http://biohackathon.org/resource/faldo#>
@@ -42,12 +42,12 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     SPARQL
 
     gene2pseudo = {}
-    gene_pseudo.each{|data|
+    gene_pseudo.each {|data|
       gene2pseudo[data[:gene]] = 1
     }
 
     ### gene - tRNA, ncRNA
-    gene_rna_product = query("#{endpoint}", <<-SPARQL.strip_heredoc)
+    gene_rna_product = query(endpoint, <<-SPARQL.strip_heredoc)
       PREFIX insdc: <http://ddbj.nig.ac.jp/ontologies/nucleotide/>
       PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
       PREFIX obo: <http://purl.obolibrary.org/obo/>
@@ -66,15 +66,15 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     SPARQL
 
     gene2rna_product = {}
-    gene_rna_product.each{|data|
+    gene_rna_product.each {|data|
       gene2rna_product[data[:gene]] = [] unless gene2rna_product[data[:gene]]
       gene2rna_product[data[:gene]].push(data[:product])
     }
 
     ### gene - citation
-    gene_citation = query("#{endpoint}", <<-SPARQL.strip_heredoc)
+    gene_citation = query(endpoint, <<-SPARQL.strip_heredoc)
       PREFIX up: <http://purl.uniprot.org/core/>
-      SELECT ?gene (COUNT(DISTINCT ?cite) AS ?citation) 
+      SELECT ?gene (COUNT(DISTINCT ?cite) AS ?citation)
       FROM <http://togogenome.org/graph/tgup>
       FROM <http://togogenome.org/graph/uniprot>
       WHERE {
@@ -86,13 +86,13 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     SPARQL
 
     gene2citation = {}
-    gene_citation.each{|data|
+    gene_citation.each {|data|
       gene2citation[data[:gene]] = data[:citation] if data[:citation]
     }
 
     ### gene - reactome root name
     # get reactome graph name
-    reactome_graph = query("#{end_reactome}", <<-SPARQL.strip_heredoc).first
+    reactome_graph = query(end_reactome, <<-SPARQL.strip_heredoc).first
       SELECT DISTINCT ?graph
       WHERE { GRAPH ?graph { ?s a <http://www.biopax.org/release/biopax-level3.owl#Pathway> . }}
     SPARQL
@@ -100,7 +100,7 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     graph = reactome_graph[:graph]
 
     # reactome_id - reactome root name
-    reactome = query("#{end_reactome}", <<-SPARQL.strip_heredoc)
+    reactome = query(end_reactome, <<-SPARQL.strip_heredoc)
       PREFIX bpx: <http://www.biopax.org/release/biopax-level3.owl#>
       SELECT DISTINCT (REPLACE( STR(?reactome), "http://identifiers.org/reactome/", "") AS ?reactome_id) ?parent_name
       FROM <#{graph}>
@@ -109,19 +109,19 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
         ?parent bpx:pathwayComponent+ ?reactome .
         OPTIONAL { ?root bpx:pathwayComponent ?parent . }
         FILTER(! REGEX(?root, "reactome"))
-        ?parent  bpx:displayName ?parent_name .    
+        ?parent  bpx:displayName ?parent_name .
       }
     SPARQL
 
     reactome2root_name = {}
-    reactome.each{|data|
+    reactome.each {|data|
       react_id = data[:reactome_id].split("\.").first
       reactome2root_name[react_id] = [] unless reactome2root_name[react_id]
       reactome2root_name[react_id].push(data[:parent_name])
     }
 
     # gene - reactome_id
-    gene_reactome = query("#{endpoint}", <<-SPARQL.strip_heredoc)
+    gene_reactome = query(endpoint, <<-SPARQL.strip_heredoc)
       PREFIX up: <http://purl.uniprot.org/core/>
       SELECT ?gene (REPLACE( STR(?reactome), "http://purl.uniprot.org/reactome/", "") AS ?reactome_id)
       FROM <http://togogenome.org/graph/tgup>
@@ -135,7 +135,7 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     SPARQL
 
     gene2reactome = {}
-    gene_reactome.each{|data|
+    gene_reactome.each {|data|
       if data[:reactome_id] && reactome2root_name[data[:reactome_id]]
         gene2reactome[data[:gene]] = [] unless gene2reactome[data[:gene]]
         gene2reactome[data[:gene]] += reactome2root_name[data[:reactome_id]]
@@ -143,7 +143,7 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
     }
 
     ### gene - protein name, ec
-    gene_protein = query("#{endpoint}", <<-SPARQL.strip_heredoc)
+    gene_protein = query(endpoint, <<-SPARQL.strip_heredoc)
       PREFIX up: <http://purl.uniprot.org/core/>
       SELECT DISTINCT ?gene ?protein_name ?ec_name
       FROM <http://togogenome.org/graph/tgup>
@@ -154,7 +154,7 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
         ?gene rdfs:seeAlso/rdfs:seeAlso ?protein .
         ?protein a up:Protein ;
                  up:recommendedName ?recommended_name_node .
-        ?recommended_name_node up:fullName ?protein_name . 
+        ?recommended_name_node up:fullName ?protein_name .
 
         OPTIONAL { ?recommended_name_node up:ecName ?ec_name . }
       }
@@ -162,30 +162,30 @@ class OrganismGeneListStanza < TogoStanza::Stanza::Base
 
     gene2protein = {}
     gene2ec = {}
-    gene_protein.each{|data|
+    gene_protein.each {|data|
       if data[:protein_name]
         gene2protein[data[:gene]] = [] unless gene2protein[data[:gene]]
-        gene2protein[data[:gene]].push(data[:protein_name]) 
+        gene2protein[data[:gene]].push(data[:protein_name])
       end
-     if data[:ec_name]
+      if data[:ec_name]
         gene2ec[data[:gene]] = [] unless gene2ec[data[:gene]]
-        gene2ec[data[:gene]].push(data[:ec_name]) 
+        gene2ec[data[:gene]].push(data[:ec_name])
       end
     }
 
-    result.each{|data|
+    result.each {|data|
       if gene2pseudo[data[:gene]]
         data.delete(:gene_id)
         data[:gene_name] += " (pseudo)"
-      end 
+      end
       data[:citation] = gene2citation[data[:gene]] if gene2citation[data[:gene]]
       data[:ec] = gene2ec[data[:gene]].uniq.join(", ") if gene2ec[data[:gene]]
       data[:reactome] =  gene2reactome[data[:gene]].uniq if gene2reactome[data[:gene]]
-       if gene2protein[data[:gene]]
-         data[:attr] = gene2protein[data[:gene]].uniq.join(", ")
-       elsif gene2rna_product[data[:gene]]
-         data[:attr] = gene2rna_product[data[:gene]].uniq.join(", ")
-       end
+      if gene2protein[data[:gene]]
+        data[:attr] = gene2protein[data[:gene]].uniq.join(", ")
+      elsif gene2rna_product[data[:gene]]
+        data[:attr] = gene2rna_product[data[:gene]].uniq.join(", ")
+      end
     }
   end
 end
