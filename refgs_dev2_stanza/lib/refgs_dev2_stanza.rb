c163ed2081b1824c0cf1a80599cac76617396f30
Takatomo Fujisawa <fujisawa.takatomo@gmail.com>
2017-05-31 18:47:35 +0900

    refgs_dev, refgs_dev2スタンザを削除

--- refgs_dev2_stanza/lib/refgs_dev2_stanza.rb
+++
@@ -1,6 +0,0 @@
-require 'togostanza'
-require_relative '../stanza'
-
-RefgsDev2Stanza.root = File.expand_path('../..', __FILE__)
-
-TogoStanza.sprockets.append_path File.expand_path('../../assets', __FILE__)
--- refgs_dev_stanza/lib/refgs_dev_stanza.rb
+++
@@ -1,6 +0,0 @@
-require 'togostanza'
-require_relative '../stanza'
-
-RefgsDevStanza.root = File.expand_path('../..', __FILE__)
-
-TogoStanza.sprockets.append_path File.expand_path('../../assets', __FILE__)
